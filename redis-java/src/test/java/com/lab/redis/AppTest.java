65d5cbbd25ff8d4a7afa9cc82377c4a5087a76d6
Wedson Quintanilha da Silva <wedson.silva@basis.com.br>
2021-03-04 19:16:33 -0300

    fix

--- redis-java/src/test/java/com/lab/redis/AppTest.java
+++ redis-java/src/test/java/com/lab/redis/AppTest.java
@@ -2,11 +2,23 @@ package com.lab.redis;
 
 import static org.junit.Assert.assertTrue;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 /**
  * Unit test for simple App.
  */
+@SpringBootTest
+@AutoConfigureMockMvc
 public class AppTest 
 {
     /**
@@ -17,4 +29,14 @@ public class AppTest
     {
         assertTrue( true );
     }
+    
+    @Autowired
+	private MockMvc mvc;
+
+	@Test
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Laboratório de REDIS no ar!\\n!")));
+	}
 }
